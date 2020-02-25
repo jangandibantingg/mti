1970c77f308d68c1e75dc4c93384fcf4b45292da
aang-naja <oredo.aang@gmail.com>
2020-02-25 09:23:57 +0700

    update selasa 25 feb

--- view/pt-data-log.php
+++ view/pt-data-log.php
@@ -1,4 +1,4 @@
-<script src="ajax/modify.pembelian.js"></script>
+
 
                     <div class="col-md-12">
 
@@ -11,9 +11,9 @@
                             </div>
                             <div class="card-body">
                               <small>
-                              <table id="config-table" class="table display no-wrap">
+                              <table id="example" class="table display no-wrap">
                                 <thead>
-                                    <tr >
+                                    <tr>
 
                                         <th>Transfer</th>
                                         <th>ID</th>
@@ -27,44 +27,22 @@
 
                                     </tr>
                                 </thead>
-                                  <tbody>
-                                    <?php
-                                      $_SESSION['from-pembelian'] = $_POST['from'];
-                                      $_SESSION['until-pembelian'] = $_POST['until'];
+                                  <tfoot>
+                                    <tr>
 
-                                      $from= $_SESSION['from-pembelian'];
-                                      $until= $_SESSION['until-pembelian'];
-                                      if (empty($_POST)) {
-                                        $p=mysqli_query($con, "SELECT * from ima_data where tanggal='$date' ");
-                                        echo "<p  align='right'><small class='font-weight-bold'>Data log hari ini -- ".date('D, d M Y')."</small></p>";
-                                      }else {
-                                        $p=mysqli_query($con, "SELECT * from ima_data where tanggal between '$from' and '$until' ");
-
-                                      }
-
-                                       while ($r=mysqli_fetch_array($p)) {
-
-                                     ?>
-                                     <?php
-
-                                          echo "<tr>
-
-                                           <td>$r[transfer]</td>
-                                           <td>$r[id_station]</td>
-                                           <td>$r[station_pengiriman]</td>
-                                           <td>$r[station_penerima]</td>
-                                           <td>$r[log_penerimaan]</td>
-                                           <td>$r[log_penerima]</td>
-                                           <td>$r[tanggal]</td>
-                                           <td>$r[status]</td>
-
-                                           </tr>";
-
-                                        }
-                                        ?>
+                                        <th>Transfer</th>
+                                        <th>ID</th>
+                                        <th>Station pengiriman</th>
+                                        <th>Station Penerima</th>
+                                        <th>Log Penerimaan</th>
+                                        <th>Log Penerima</th>
+                                        <th>Tanggal</th>
+                                        <th>Status</th>
 
 
-                                  </tbody>
+                                    </tr>
+
+                                  </tfoot>
 
                               </table>
                               </small>
@@ -74,4 +52,16 @@
 
 
 
-                          </div>
+                </div>
+<script type="text/javascript">
+$(document).ready(function() {
+  $('#example').DataTable( {
+      "responsive": true,
+      "processing": false,
+      "serverSide": true,
+      "ajax": "server_side/datalog.php"
+  } );
+
+  $.fn.dataTable.ext.errMode = 'none';
+} );
+</script>
