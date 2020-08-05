e59cff8b6241fc45f992d715fa3b633aeaf23406
jangandibantingg <oredo.aang@gmail.com>
2020-08-05 11:51:58 +0700

    update number bar chart

--- print/beranda.php
+++ print/beranda.php
@@ -25,10 +25,6 @@ $q=mysqli_fetch_array(mysqli_query($con, "select nama_station from ima_station w
 				 <link href="../library/material/dist/css/pages/floating-label.css" rel="stylesheet">
 
 
-         <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">
-         <!-- <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script> -->
-         <script src="http://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-         <script src="http://cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
 
          <style media="screen">
            #morris-bar-chart-dashboard{
@@ -74,29 +70,108 @@ $q=mysqli_fetch_array(mysqli_query($con, "select nama_station from ima_station w
 
 
 
-	<script type="text/javascript">
+	<!-- <script type="text/javascript">
 	window.print();
 	window.onfocus=function(){ window.close();}
-	</script>
-	<!-- This is data table -->
-<script src="https://cdn.datatables.net/buttons/1.5.1/js/dataTables.buttons.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.flash.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/pdfmake.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.html5.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.print.min.js"></script>
-<script src="../ajax/datatables.js"></script>
+	</script> -->
 
-<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script> -->
+
+
+	<!-- This morris js -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
+  <link href="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css" rel="stylesheet" />
+
+
 
 <?php require "../chart/bar-status-station.php"; ?>
-<!-- <?php require "../chart/donute-total-transfer.php"; ?> -->
-
 
 <script type="text/javascript">
 
 $(function () {
+
+(function() {
+var $, MyMorris;
+
+MyMorris = window.MyMorris = {};
+$ = jQuery;
+
+MyMorris = Object.create(Morris);
+
+MyMorris.Bar.prototype.defaults["labelTop"] = false;
+
+MyMorris.Bar.prototype.drawLabelTop = function(xPos, yPos, text) {
+  var label;
+  return label = this.raphael.text(xPos, yPos, text).attr('font-size', this.options.gridTextSize).attr('font-family', this.options.gridTextFamily).attr('font-weight', this.options.gridTextWeight).attr('fill', this.options.gridTextColor);
+};
+
+MyMorris.Bar.prototype.drawSeries = function() {
+  var barWidth, bottom, groupWidth, idx, lastTop, left, leftPadding, numBars, row, sidx, size, spaceLeft, top, ypos, zeroPos;
+  groupWidth = this.width / this.options.data.length;
+  numBars = this.options.stacked ? 1 : this.options.ykeys.length;
+  barWidth = (groupWidth * this.options.barSizeRatio - this.options.barGap * (numBars - 1)) / numBars;
+  if (this.options.barSize) {
+    barWidth = Math.min(barWidth, this.options.barSize);
+  }
+  spaceLeft = groupWidth - barWidth * numBars - this.options.barGap * (numBars - 1);
+  leftPadding = spaceLeft / 2;
+  zeroPos = this.ymin <= 0 && this.ymax >= 0 ? this.transY(0) : null;
+  return this.bars = (function() {
+    var _i, _len, _ref, _results;
+    _ref = this.data;
+    _results = [];
+    for (idx = _i = 0, _len = _ref.length; _i < _len; idx = ++_i) {
+      row = _ref[idx];
+      lastTop = 0;
+      _results.push((function() {
+        var _j, _len1, _ref1, _results1;
+        _ref1 = row._y;
+        _results1 = [];
+        for (sidx = _j = 0, _len1 = _ref1.length; _j < _len1; sidx = ++_j) {
+          ypos = _ref1[sidx];
+          if (ypos !== null) {
+            if (zeroPos) {
+              top = Math.min(ypos, zeroPos);
+              bottom = Math.max(ypos, zeroPos);
+            } else {
+              top = ypos;
+              bottom = this.bottom;
+            }
+            left = this.left + idx * groupWidth + leftPadding;
+            if (!this.options.stacked) {
+              left += sidx * (barWidth + this.options.barGap);
+            }
+            size = bottom - top;
+            if (this.options.verticalGridCondition && this.options.verticalGridCondition(row.x)) {
+              this.drawBar(this.left + idx * groupWidth, this.top, groupWidth, Math.abs(this.top - this.bottom), this.options.verticalGridColor, this.options.verticalGridOpacity, this.options.barRadius, row.y[sidx]);
+            }
+            if (this.options.stacked) {
+              top -= lastTop;
+            }
+            this.drawBar(left, top, barWidth, size, this.colorFor(row, sidx, 'bar'), this.options.barOpacity, this.options.barRadius);
+            _results1.push(lastTop += size);
+
+            if (this.options.labelTop && !this.options.stacked) {
+              label = this.drawLabelTop((left + (barWidth / 2)), top - 10, row.y[sidx]);
+              textBox = label.getBBox();
+              _results.push(textBox);
+            }
+          } else {
+            _results1.push(null);
+          }
+        }
+        return _results1;
+      }).call(this));
+    }
+    return _results;
+  }).call(this);
+};
+}).call(this);
+
+
+
+
 Morris.Bar({
  element: 'morris-bar-chart-dashboard',
 
@@ -114,13 +189,16 @@ Morris.Bar({
   return y = Math.round(y);
 },
 
- xLabelAngle:90,
- verticalGrid: true,
- padding: 75,
- xLabelMargin : 20,
- gridTextSize:11,
- numLines: 20,
- onlyIntegers: true,
+barColors: ['#48C9B0', '#CD6155'],
+xLabelAngle:90,
+verticalGrid: true,
+padding: 75,
+xLabelMargin : 20,
+gridTextSize:12,
+numLines: 20,
+onlyIntegers: true,
+gridTextFamily:'Arial Black',
+labelTop: true,
 
 });
 // donat
--- print/nama-station.php
+++ print/nama-station.php
@@ -12,7 +12,7 @@ $q=mysqli_fetch_array(mysqli_query($con, "select nama_station from ima_station w
 <head>
 	<title> PT MITRA TEKNOLOGI IMA</title>
 
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+	<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script> -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
   <!-- <link rel="stylesheet" href="./a4.css"> -->
 
@@ -29,18 +29,17 @@ $q=mysqli_fetch_array(mysqli_query($con, "select nama_station from ima_station w
 
 
 
-                  <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">
-                  <!-- <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script> -->
-                  <script src="http://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-                  <script src="http://cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
+
 
 
          <style media="screen">
            #morris-bar-chart-dashboard{
              height:1500px;
+             font-weight: bold;
            }
            #morris-bar-chart{
              height:1200px;
+             font-weight: 900;
            }
          </style>
 
@@ -235,13 +234,107 @@ $q=mysqli_fetch_array(mysqli_query($con, "select nama_station from ima_station w
 	window.onfocus=function(){ window.close();}
 	</script>
 
+
+<!--  -->
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
+<link href="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css" rel="stylesheet" />
+
+
+<!--  -->
+
+
+
 <?php require "../chart/bar-status-station.php"; ?>
 <!-- <?php require "../chart/donute-total-transfer.php"; ?> -->
 <script type="text/javascript">
 $(function () {
 
+  (function() {
+    var $, MyMorris;
 
-    // Morris bar chart
+    MyMorris = window.MyMorris = {};
+    $ = jQuery;
+
+    MyMorris = Object.create(Morris);
+
+    MyMorris.Bar.prototype.defaults["labelTop"] = false;
+
+    MyMorris.Bar.prototype.drawLabelTop = function(xPos, yPos, text) {
+      var label;
+      return label = this.raphael.text(xPos, yPos, text).attr('font-size', this.options.gridTextSize).attr('font-family', this.options.gridTextFamily).attr('font-weight', this.options.gridTextWeight).attr('fill', this.options.gridTextColor);
+    };
+
+    MyMorris.Bar.prototype.drawSeries = function() {
+      var barWidth, bottom, groupWidth, idx, lastTop, left, leftPadding, numBars, row, sidx, size, spaceLeft, top, ypos, zeroPos;
+      groupWidth = this.width / this.options.data.length;
+      numBars = this.options.stacked ? 1 : this.options.ykeys.length;
+      barWidth = (groupWidth * this.options.barSizeRatio - this.options.barGap * (numBars - 1)) / numBars;
+      if (this.options.barSize) {
+        barWidth = Math.min(barWidth, this.options.barSize);
+      }
+      spaceLeft = groupWidth - barWidth * numBars - this.options.barGap * (numBars - 1);
+      leftPadding = spaceLeft / 2;
+      zeroPos = this.ymin <= 0 && this.ymax >= 0 ? this.transY(0) : null;
+      return this.bars = (function() {
+        var _i, _len, _ref, _results;
+        _ref = this.data;
+        _results = [];
+        for (idx = _i = 0, _len = _ref.length; _i < _len; idx = ++_i) {
+          row = _ref[idx];
+          lastTop = 0;
+          _results.push((function() {
+            var _j, _len1, _ref1, _results1;
+            _ref1 = row._y;
+            _results1 = [];
+            for (sidx = _j = 0, _len1 = _ref1.length; _j < _len1; sidx = ++_j) {
+              ypos = _ref1[sidx];
+              if (ypos !== null) {
+                if (zeroPos) {
+                  top = Math.min(ypos, zeroPos);
+                  bottom = Math.max(ypos, zeroPos);
+                } else {
+                  top = ypos;
+                  bottom = this.bottom;
+                }
+                left = this.left + idx * groupWidth + leftPadding;
+                if (!this.options.stacked) {
+                  left += sidx * (barWidth + this.options.barGap);
+                }
+                size = bottom - top;
+                if (this.options.verticalGridCondition && this.options.verticalGridCondition(row.x)) {
+                  this.drawBar(this.left + idx * groupWidth, this.top, groupWidth, Math.abs(this.top - this.bottom), this.options.verticalGridColor, this.options.verticalGridOpacity, this.options.barRadius, row.y[sidx]);
+                }
+                if (this.options.stacked) {
+                  top -= lastTop;
+                }
+                this.drawBar(left, top, barWidth, size, this.colorFor(row, sidx, 'bar'), this.options.barOpacity, this.options.barRadius);
+                _results1.push(lastTop += size);
+
+                if (this.options.labelTop && !this.options.stacked) {
+                  label = this.drawLabelTop((left + (barWidth / 2)), top - 10, row.y[sidx]);
+                  textBox = label.getBBox();
+                  _results.push(textBox);
+                }
+              } else {
+                _results1.push(null);
+              }
+            }
+            return _results1;
+          }).call(this));
+        }
+        return _results;
+      }).call(this);
+    };
+  }).call(this);
+
+
+
+
+
+
 Morris.Bar({
 element: 'morris-bar-chart',
 data: <?php echo json_encode($arr_station); ?>,
@@ -256,12 +349,14 @@ xLabelAngle:90,
 verticalGrid: true,
 padding: 75,
 xLabelMargin : 20,
-gridTextSize:11,
+gridTextSize:12,
 numLines: 20,
 onlyIntegers: true,
+gridTextFamily:'Arial Black',
+labelTop: true,
+
+
 });
-
-
 
 
 
